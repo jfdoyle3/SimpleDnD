93173b2bd6fd9fc62fe47bd4f7d99275de81d889
Jim Doyle <indellibleink@gmail.com>
2019-08-20 19:39:31 -0400

    Add project files.

---
+++ SimpleDnD/Die.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleDnD
+{
+    class Die
+    {
+        public int Faces;
+        public int FaceUp;
+       
+        public Die(int faces)
+        {
+            Random cast = new Random();
+            this.Faces= faces;
+            this.FaceUp = cast.Next(1, faces); 
+        }
+
+        public Die(int faces, int faceUp)
+        {
+            this.Faces = faces;
+            this.FaceUp = faceUp;
+        }
+        private bool isValid()
+        {
+            this.Faces = 0;
+            return true;
+        }
+    }
+}
---
+++ SimpleDnD/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SimpleDnD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Die d20 = new Die(20);
+
+            Console.WriteLine(d20.FaceUp);
+
+            int modifer = Convert.ToInt32(Console.Read());
+
+        }
+    }
+}
